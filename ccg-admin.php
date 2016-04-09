f40a1be18c730eb7407a008e482b4a798a423ac8
Aharon Varady <aharon@opensiddur.org>
2016-04-09 01:33:02 -0400

    image name link fix

--- ccg-admin.php
+++ ccg-admin.php
@@ -79,16 +79,16 @@ function ccg_get_banner( $ccg_options, $who_call ) {
 		$attributes = "fu";
 		$version = "";
 		$versiontype = "";
-		$image_attributes = "copyright+fair-use";
+		$image_attributes = "C-FU";
 		$cctype = "right";
 		$attributes_text = __( 'United States Copyright Law: Fair Use Right', 'ccg-domain' );
 	}
 	
 	if ( $ccg_options['mod'] == "fu" ) {
-	$banner = "<a target='_blank' rel='license nofollow' href='https://www.law.cornell.edu/uscode/text/17/107'><img class='alignright' alt='17 U.S. Code  107 - Limitations on exclusive rights: Fair use' style='border-width:0' src='http://opensiddur.org/wp-content/images/" . $image_attributes . ".svg.150x100.png' /></a><br />";
+	$banner = "<a target='_blank' rel='license nofollow' href='https://www.law.cornell.edu/uscode/text/17/107'><img class='alignright' alt='17 U.S. Code  107 - Limitations on exclusive rights: Fair use' style='border-width:0' src='http://opensiddur.org/wp-content/images/" . $image_attributes . ".svg.50x150.png' /></a><br />";
 	$islicensed = ' under the contributor&#39;s <a target="_blank" rel="license nofollow" href="https://www.law.cornell.edu/uscode/text/17/107">Fair Use Right</a> (17 U.S. Code  107 - Limitations on exclusive rights: Fair use), in respect to the copyrighted material included. Any additional work that is not already in the Public Domain is shared under a Creative Commons Attribution 4.0 Unported license.<br />';
 	} else {
-	$banner = "<a target='_blank' rel='license nofollow' href='http://creativecommons.org/" . $licensedir . '/' . $attributes . '/' . $version ."/'><img class='alignright' alt=' . $attributes_text . $version . $versiontype .' style='border-width:0' src='http://opensiddur.org/wp-content/images/" . $image_attributes . ".svg.150x100.png' /></a><br />";
+	$banner = "<a target='_blank' rel='license nofollow' href='http://creativecommons.org/" . $licensedir . '/' . $attributes . '/' . $version ."/'><img class='alignright' alt=' . $attributes_text . $version . $versiontype .' style='border-width:0' src='http://opensiddur.org/wp-content/images/" . $image_attributes . ".svg.50x150.png' /></a><br />";
 	$islicensed = ' with a <a target="_blank" rel="license nofollow" href="http://creativecommons.org/' . $licensedir . '/' . $attributes . '/' . $version .'/">' . $attributes_text . '&nbsp;' . $version . '&nbsp;' . $versiontype .'</a> ' . $cctype . '.<br />';
 	}
 	
